1545f3ee32851588c64d22f0ad1e0e96e03772a5
ink169 <andrew.ingpen@gmail.com>
2025-02-08 14:07:02 +0000

    Added viewmodel to index2

--- Proventeq-AzureSaasPricing/Pages/Index2.cshtml.cs
+++ Proventeq-AzureSaasPricing/Pages/Index2.cshtml.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Proventeq_AzureSaasPricing.Models.ViewModels;
 
 namespace Proventeq_AzureSaasPricing.Pages
 {
     public class Index2Model : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        public PricingViewModel PricingViewModel { get; set; }
 
         public Index2Model(ILogger<IndexModel> logger)
         {
